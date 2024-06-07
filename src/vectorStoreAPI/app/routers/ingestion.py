43a9f42ff6234a7581705683d66d8be6943b8bbe
Vladislav Sveltakov <svetlak0f@yandex.by>
2024-06-07 22:54:29 +0300

    refactoring

--- src/vectorStoreAPI/app/routers/ingestion.py
+++ src/vectorStoreAPI/app/routers/ingestion.py
@@ -3,12 +3,13 @@ import requests
 import os
 from uuid import UUID, uuid4
 from typing import Annotated, Optional
-import os
 import magic
 
 from ..internal.vectorizer.main import ResnetVectorizer
 from ..internal.qdrant_handler import VectorHandler
 
+
+blob_directory = "./data/videos/"
 
 video_vectorizer = ResnetVectorizer(device="mps")
 video_db_handler = VectorHandler()
@@ -18,18 +19,23 @@ router = APIRouter()
 @router.post("/upload_video")
 async def upload_video(video: UploadFile = File(...)):
     # Specify the directory where you want to save the video
-    save_directory = "./data/videos/"
-
-    # file_type = magic.from_buffer(await video.read(), mime=True)
-    # if file_type != 'video/mp4':
-    #     return {"message": "Invalid video format. Only MP4 videos are allowed."}
 
     # Save the video file to disk
-    with open(save_directory + video.filename, "wb") as f:
+    save_path = blob_directory + video.filename,
+    with open(save_path, "wb") as f:
         f.write(await video.read())
 
+    try:
+        result = video_vectorizer.process_video(video_path=save_path)
+    except:
+        os.remove(save_path)
+        raise HTTPException(422, "Error while processing, check media format")
     
-    result = video_vectorizer.process_video(video_path=save_directory + video.filename)
-    video_db_handler.save_vectors(result.tolist(), video_id=video.filename)
+    try:
+        video_db_handler.save_vectors(result.tolist(), video_id=video.filename)
+    except Exception as e:
+        os.remove(save_path)
+        raise HTTPException(500, f"Error while vector store uploading, details {e}")
+
 
     return {"message": "Video saved successfully"}
