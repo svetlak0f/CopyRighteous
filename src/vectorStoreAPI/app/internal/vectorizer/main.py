cad8ba196b024b78f55427860b40b87440ab70c3
Vladislav Sveltakov <svetlak0f@yandex.by>
2024-06-07 22:48:39 +0300

    add basic video indexing option

--- src/vectorStoreAPI/app/internal/vectorizer/main.py
+++ src/vectorStoreAPI/app/internal/vectorizer/main.py
@@ -3,50 +3,62 @@ import numpy as np
 import torch
 from tqdm import tqdm
 
+from abc import ABC, abstractmethod
+
 import cv2
 
 
-model = models.resnet152(pretrained=True)
-model.eval()
 
-preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
+class AbstractVideoVectorizer(ABC):
+    @abstractmethod
+    def process_video(self, video_path: str) -> np.ndarray:
+        pass
 
-def process_image(input_batch):
 
-    if torch.cuda.is_available():
-        input_batch = input_batch.to('cuda')
-        model.to('cuda')
+class ResnetVectorizer(AbstractVideoVectorizer):
 
-    with torch.no_grad():
-        output = model(input_batch)
+    preprocess = transforms.Compose([
+                                    transforms.ToTensor(),
+                                    transforms.Resize(256),
+                                    transforms.CenterCrop(224),
+                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                                ])
+    
+    def __init__(self, device: str, inference_batch_size: int = 8):
+        self.device = torch.device(device)
+        self.model = models.resnet152(pretrained=True)
+        self.batch_size = inference_batch_size
 
-    return output
+        self.model.eval()
+        self.model.to(self.device)
 
-def process_video(video, batch_size):
 
-    cap = cv2.VideoCapture(video)
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    def process_batch(self, batch):
+        batch = batch.to(self.device)
+        with torch.no_grad():
+            output = self.model(batch)
 
-    result = np.zeros((total_frames, 1000))
-    batch_frames = []
+        return output
 
-    for frame_num in tqdm(range(1, total_frames+1), desc='Processing video'):
-        
-        ret, frame = cap.read()
-        input_tensor = preprocess(frame)
-        batch_frames.append(input_tensor)
-        batch = torch.stack(batch_frames)
-        
-        if frame_num % batch_size == 0:
-            tn = process_image(batch).cpu().numpy()
-            result[frame_num-batch_size:min(frame_num, total_frames+1)] = tn
-            batch_frames = []
+    def process_video(self, video_path: str) -> np.ndarray:
+        cap = cv2.VideoCapture(video_path)
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        print(total_frames)
 
-    cap.release()
+        result = np.zeros((total_frames, 1000), dtype=np.float32)
+        batch_frames = []
 
-    return result
+        for frame_num in tqdm(range(1, total_frames+1), desc='Processing video'):
+            ret, frame = cap.read()
+            input_tensor = self.preprocess(frame)
+            batch_frames.append(input_tensor)
+            batch = torch.stack(batch_frames)
+            
+            if frame_num % self.batch_size == 0:
+                tn = self.process_batch(batch).cpu().numpy()
+                result[frame_num-self.batch_size:min(frame_num, total_frames+1)] = tn
+                batch_frames = []
+
+        cap.release()
+
+        return result
--- src/vectorStoreAPI/app/routers/ingestion.py
+++ src/vectorStoreAPI/app/routers/ingestion.py
@@ -6,20 +6,30 @@ from typing import Annotated, Optional
 import os
 import magic
 
+from ..internal.vectorizer.main import ResnetVectorizer
+from ..internal.qdrant_handler import VectorHandler
+
+
+video_vectorizer = ResnetVectorizer(device="mps")
+video_db_handler = VectorHandler()
 
 router = APIRouter()
 
-@router.post("/upload_video/")
+@router.post("/upload_video")
 async def upload_video(video: UploadFile = File(...)):
     # Specify the directory where you want to save the video
     save_directory = "./data/videos/"
-    
-    file_type = magic.from_buffer(await video.read(), mime=True)
-    if file_type != 'video/mp4':
-        return {"message": "Invalid video format. Only MP4 videos are allowed."}
+
+    # file_type = magic.from_buffer(await video.read(), mime=True)
+    # if file_type != 'video/mp4':
+    #     return {"message": "Invalid video format. Only MP4 videos are allowed."}
 
     # Save the video file to disk
     with open(save_directory + video.filename, "wb") as f:
         f.write(await video.read())
+
     
+    result = video_vectorizer.process_video(video_path=save_directory + video.filename)
+    video_db_handler.save_vectors(result.tolist(), video_id=video.filename)
+
     return {"message": "Video saved successfully"}
