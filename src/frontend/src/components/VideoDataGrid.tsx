ece9f75113b751dca5dcec5d9a0ef4b100b0ac59
Vladislav Sveltakov <svetlak0f@yandex.by>
2024-06-10 21:06:10 +0300

    add style

--- src/frontend/src/components/VideoDataGrid.tsx
+++ src/frontend/src/components/VideoDataGrid.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
 import DeleteIcon from '@mui/icons-material/Delete';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+
+import { getAllVideosMetadata } from 'api/video_metadata';
+import { deleteVideoMetadata } from 'api/video_metadata';
 
 interface VideoData {
   video_id: string;
@@ -13,15 +20,48 @@ interface VideoData {
   framerate: number;
 }
 
-interface VideoDataGridProps {
-  data: VideoData[];
-}
+const VideoDataGrid: React.FC = () => {
+  const [data, setData] = useState<VideoData[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [deleteVideoId, setDeleteVideoId] = useState('');
+  const [isDeleteConfirmationOpen, setIsDeleteConfirmationOpen] = useState(false);
 
-const VideoDataGrid: React.FC<VideoDataGridProps> = ({ data }) => {
-  const handleDelete = (videoId: string) => {
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await getAllVideosMetadata();
+      const updatedData = response.map((video: object, index: number) => ({
+        ...video,
+        id: index + 1,
+      }));
+      setData(updatedData);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching video metadata:', error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleDelete = async (videoId: string) => {
     const confirmed = window.confirm(`Are you sure you want to delete ${videoId}?`);
     if (confirmed) {
-      // Perform delete operation
+      setIsLoading(true);
+      setDeleteVideoId(videoId);
+      setIsDeleteConfirmationOpen(true);
+      try {
+        // Perform delete operation
+        const result = await deleteVideoMetadata(videoId);
+        fetchData();
+      } catch (error) {
+        console.error('Error deleting video metadata:', error);
+      } finally {
+        setIsLoading(false);
+        setIsDeleteConfirmationOpen(false);
+      }
     }
   };
 
@@ -31,31 +71,54 @@ const VideoDataGrid: React.FC<VideoDataGridProps> = ({ data }) => {
       color="secondary"
       startIcon={<DeleteIcon />}
       onClick={() => handleDelete(params.row.video_id as string)}
-       // Add condition to enable/disable the delete button
+      disabled={isLoading}
     >
       Delete
     </Button>
   );
 
+  const renderStatusIcon = (params: GridRenderCellParams) => {
+    const status = params.row.status as string;
+    if (status === 'Indexed') {
+      return <CheckCircleIcon style={{ color: 'green' }} />;
+    }
+    return null;
+  };
+
   const columns: GridColDef[] = [
     { field: 'video_id', headerName: 'Video ID', width: 200 },
-    { field: 'uploaded_at', headerName: 'Uploaded At', width: 200 },
-    { field: 'indexed_at', headerName: 'Indexed At', width: 200 },
-    { field: 'status', headerName: 'Status', width: 120 },
-    { field: 'frames_count', headerName: 'Frames Count', width: 150 },
-    { field: 'video_time', headerName: 'Video Time', width: 150 },
-    { field: 'framerate', headerName: 'Frame Rate', width: 120 },
+    { field: 'uploaded_at', headerName: 'Загружено', width: 250 },
+    { field: 'indexed_at', headerName: 'Индексировано', width: 250 },
+    { field: 'status', headerName: 'Статус', width: 120 },
+    { field: 'frames_count', headerName: 'Количество кадров', width: 150 },
+    { field: 'video_time', headerName: 'Продолжительность видео', width: 200 },
+    { field: 'framerate', headerName: 'Частота кадров', width: 120 },
+    {
+      field: 'status_icon',
+      headerName: '',
+      width: 60,
+      renderCell: renderStatusIcon,
+    },
     {
       field: 'delete',
-      headerName: 'Delete',
+      headerName: 'Удалить',
       width: 140,
       renderCell: renderDeleteButton,
     },
   ];
 
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: 800, width: '100%' }}>
       <DataGrid rows={data} columns={columns} />
+
+      <Dialog open={isDeleteConfirmationOpen} onClose={() => setIsDeleteConfirmationOpen(false)}>
+        <DialogContent>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <CircularProgress size={20} style={{ marginRight: '10px' }} />
+            <span>Удаление видео {deleteVideoId}...</span>
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
