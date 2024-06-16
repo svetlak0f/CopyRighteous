71530c4ee5242a8df610aa72edc8843419232ab1
Vladislav Sveltakov <svetlak0f@yandex.by>
2024-06-16 18:06:42 +0300

    fix address

--- src/frontend/src/constants/apiAddress.ts
+++ src/frontend/src/constants/apiAddress.ts
@@ -1,2 +1,2 @@
-export const apiAddress= "http://localhost:8000"
-// export const apiAddress= "http://aqua-monitoring.ru:8000"
+// export const apiAddress= "http://localhost:8000"
+export const apiAddress= "http://212.109.194.116:8000"
